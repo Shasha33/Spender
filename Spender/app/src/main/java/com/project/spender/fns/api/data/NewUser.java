b7caf8606af2022936ad86548eace2644c37aa60
Michael <michael.nik999@gmail.com>
2019-05-25 16:51:07 +0300

    add javadoc

--- Spender/app/src/main/java/com/project/spender/fns/api/data/NewUser.java
+++ Spender/app/src/main/java/com/project/spender/fns/api/data/NewUser.java
@@ -1,5 +1,10 @@
 package com.project.spender.fns.api.data;
 
+/**
+ * Объект для нового пользователя.
+ * emain и name используются только при регистрации и больше нигде.
+ * Формат номера: +79991234567
+ */
 public class NewUser {
     public String emain;
     public String name;
