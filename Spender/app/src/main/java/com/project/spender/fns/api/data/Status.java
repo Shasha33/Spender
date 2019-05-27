a86e1a40162b35f3d2dfea23366e400faf8c0383
Darya Razumova <d.razumova@yandex.ru>
2019-05-28 00:55:06 +0300

    defiled the crown of creation

--- Spender/app/src/main/java/com/project/spender/fns/api/data/Status.java
+++ Spender/app/src/main/java/com/project/spender/fns/api/data/Status.java
@@ -1,8 +1,8 @@
 package com.project.spender.fns.api.data;
 
 public enum Status {
-    SUCCESS,
-    EXIST,
-    ERROR,
-    SENDING
+    SUCCESS, //check received
+    EXIST, //check exists, but not received yet
+    ERROR, //error occurred
+    SENDING //request for existing
 }
