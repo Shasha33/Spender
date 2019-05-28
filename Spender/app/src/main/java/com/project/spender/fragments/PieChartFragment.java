eb5c2e50e12efc4313f5b77024dcb6727eadfbd5
Michael <michael.nik999@gmail.com>
2019-05-29 02:55:23 +0300

    add pie/donat toggle

--- Spender/app/src/main/java/com/project/spender/fragments/PieChartFragment.java
+++ Spender/app/src/main/java/com/project/spender/fragments/PieChartFragment.java
@@ -48,4 +48,7 @@ public class PieChartFragment extends Fragment {
         return view;
     }
 
+    public PieChartController getPieChartController() {
+        return pieChartController;
+    }
 }
