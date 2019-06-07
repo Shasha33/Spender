0e816b72d887afcbd60884a930fd4b380e5b97eb
Michael <michael.nik999@gmail.com>
2019-06-07 17:09:07 +0300

    add getUserReadableMassage

--- Spender/app/src/main/java/com/project/spender/fns/api/data/CheckJsonWithStatus.java
+++ Spender/app/src/main/java/com/project/spender/fns/api/data/CheckJsonWithStatus.java
@@ -6,27 +6,16 @@ import com.project.spender.fns.api.exception.NetworkException;
 /**
  * Класс для отображения статуса запроса и возврашения результата или исключения.
  */
-public class CheckJsonWithStatus {
+public class CheckJsonWithStatus extends StatusWithResponse{
 
     private CheckJson checkJson;
-    private Status status;
-    private NetworkException exception;
 
     public CheckJsonWithStatus(CheckJson checkJson, Status status, NetworkException exception) {
+        super(status, exception);
         this.checkJson = checkJson;
-        this.status = status;
-        this.exception = exception;
     }
 
     public CheckJson getCheckJson() {
         return checkJson;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public NetworkException getException() {
-        return exception;
     }
 }
